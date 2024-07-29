0e03a07b84d20d4be30691f56a31b232136a86d2
Savastane <savastane@gmail.com>
2024-07-28 23:56:33 -0300

    fix item menu

--- components/Header/NavComponent.vue
+++ components/Header/NavComponent.vue
@@ -12,30 +12,29 @@
            <nav>
                <ul class="no-list-style">
                    <li>
-                       <a href="https://townhub.kwst.net/listing5.html#">Estética <i class="fa fa-caret-down"></i></a>
+                       <a href="#">Estética <i class="fa fa-caret-down"></i></a>
                        <!--second level -->   
                        <ul>
-                           <li><a href="https://townhub.kwst.net/index.html">Parall1ax Image</a></li>
-                           <li><a href="https://townhub.kwst.net/index2.html">Slider</a></li>
-                           <li><a href="https://townhub.kwst.net/index3.html">Slideshow</a></li>
-                           <li><a href="https://townhub.kwst.net/index4.html">Video</a></li>
-                           <li><a href="https://townhub.kwst.net/index5.html">Map</a></li>
-                           <li><a href="https://townhub.kwst.net/rtl/index.html" target="_blank">RTL Version</a></li>
+                           <li><a href="#">Podologia</a></li>
+                           <li><a href="#">Unhas</a></li>
+                           <li><a href="#">Botox</a></li>
+                           <li><a href="#">Harmonização</a></li>
+                           <li><a href="#">Infiltração</a></li>
+                           
                        </ul>
                        <!--second level end-->
                    </li>
                    <li>
-                       <a href="https://townhub.kwst.net/listing5.html#" class="act-link">Tattos <i class="fa fa-caret-down"></i></a>
+                       <a href="#" class="act-link">Tattos <i class="fa fa-caret-down"></i></a>
                        <!--second level -->
                        <ul>
-                           <li><a href="https://townhub.kwst.net/listing.html">Column map</a></li>
-                           <li><a href="https://townhub.kwst.net/listing2.html">Column map 2</a></li>
-                           <li><a href="https://townhub.kwst.net/listing3.html">Fullwidth Map</a></li>
-                           <li><a href="https://townhub.kwst.net/listing4.html">Fullwidth Map 2</a></li>
-                           <li><a href="https://townhub.kwst.net/listing5.html">Without Map</a></li>
-                           <li><a href="https://townhub.kwst.net/listing6.html">Without Map 2</a></li>
+                           <li><a href="#">Old School</a></li>
+                           <li><a href="#">Tribal</a></li>
+                           <li><a href="#">Oriental</a></li>
+                           <li><a href="#">Biomecânico</a></li>
+                           <li><a href="#">Neo Tradicional</a></li>
                            <li>
-                               <a href="https://townhub.kwst.net/listing5.html#">Single <i class="fa fa-caret-down"></i></a>
+                               <a href="#">Single <i class="fa fa-caret-down"></i></a>
                                <!--third  level  -->
                                <ul>
                                    <li><a href="https://townhub.kwst.net/listing-single.html">Style 1</a></li>
@@ -49,26 +48,26 @@
                        <!--second level end-->
                    </li>
                    <li>
-                       <a href="https://townhub.kwst.net/blog.html">Pets</a>
+                       <a href="#">Pets</a>
                    </li>
                    <li>
-                       <a href="https://townhub.kwst.net/listing5.html#">Terapias <i class="fa fa-caret-down"></i></a>
+                       <a href="#">Terapias <i class="fa fa-caret-down"></i></a>
                        <!--second level -->   
                        <ul>
                            <li>
-                               <a href="https://townhub.kwst.net/listing5.html#">Shop<i class="fa fa-caret-down"></i></a>
+                               <a href="#">Orientais<i class="fa fa-caret-down"></i></a>
                                <!--third  level  -->
                                <ul>
-                                   <li><a href="https://townhub.kwst.net/shop.html">Products</a></li>
-                                   <li><a href="https://townhub.kwst.net/product-single.html">Product single</a></li>
-                                   <li><a href="https://townhub.kwst.net/cart.html">Cart</a></li>
+                                   <li><a href="#">Aculputura</a></li>
+                                   <li><a href="#">Tai Chua</a></li>
+                                   <li><a href="#">Oriental Massagens</a></li>
                                </ul>
                                <!--third  level end-->
                            </li>
-                           <li><a href="https://townhub.kwst.net/about.html">About</a></li>
-                           <li><a href="https://townhub.kwst.net/contacts.html">Contacts</a></li>
-                           <li><a href="https://townhub.kwst.net/author-single.html">User single</a></li>
-                           <li><a href="https://townhub.kwst.net/help.html">How it Works</a></li>
+                           <li><a href="#">RPG</a></li>
+                           <li><a href="#">Pilates</a></li>
+                           <li><a href="#">Fisioterapia</a></li>
+                           <!-- <li><a href="https://townhub.kwst.net/help.html">How it Works</a></li>
                            <li><a href="https://townhub.kwst.net/booking.html">Booking</a></li>
                            <li><a href="https://townhub.kwst.net/pricing-tables.html">Pricing</a></li>
                            <li><a href="https://townhub.kwst.net/dashboard.html">User Dasboard</a></li>
@@ -76,7 +75,7 @@
                            <li><a href="https://townhub.kwst.net/dashboard-add-listing.html">Add Listing</a></li>
                            <li><a href="https://townhub.kwst.net/invoice.html">Invoice</a></li>
                            <li><a href="https://townhub.kwst.net/login-sign.html">login Sign</a></li>
-                           <li><a href="https://townhub.kwst.net/404.html">404</a></li>
+                           <li><a href="https://townhub.kwst.net/404.html">404</a></li> -->
                        </ul>
                        <!--second level end-->                                
                    </li>
