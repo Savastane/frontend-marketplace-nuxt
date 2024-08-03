1667ef9af4ce05fdefdaccd8d87b874c98bc02c2
Savastane <savastane@gmail.com>
2024-08-02 23:41:54 -0300

    fix buy page wizard address

--- components/Buy/Wizard/Tab2Component.vue
+++ components/Buy/Wizard/Tab2Component.vue
@@ -5,11 +5,11 @@
                                                                 </div>
                                                                 <div class="row">
                                                                     <div class="col-sm-6">
-                                                                        <label class="vis-label">City <i class="fal fa-globe-asia"></i></label>
+                                                                        <label class="vis-label">Cidade <i class="fal fa-globe-asia"></i></label>
                                                                         <input type="text" placeholder="Your city" value=""/>                                                  
                                                                     </div>
                                                                     <div class="col-sm-6">
-                                                                        <label class="vis-label">Country </label>
+                                                                        <label class="vis-label">Pais </label>
                                                                         <div class="listsearch-input-item ">
                                                                             <select data-placeholder="Your Country" class="chosen-select no-search-select" >
                                                                                 <option>United states</option>
@@ -24,13 +24,13 @@
                                                                 </div>
                                                                 <div class="row">
                                                                     <div class="col-sm-12">
-                                                                        <label class="vis-label">Street <i class="fal fa-road"></i> </label>
+                                                                        <label class="vis-label">Rua <i class="fal fa-road"></i> </label>
                                                                         <input type="text" placeholder="Your Street" value=""/>                                                  
                                                                     </div>
                                                                 </div>
                                                                 <div class="row">
                                                                     <div class="col-sm-8">
-                                                                        <label class="vis-label">State<i class="fal fa-street-view"></i></label>
+                                                                        <label class="vis-label">Estado<i class="fal fa-street-view"></i></label>
                                                                         <input type="text" placeholder="Your State" value=""/>                                                  
                                                                     </div>
                                                                     <div class="col-sm-4">
@@ -39,11 +39,11 @@
                                                                     </div>
                                                                 </div>
                                                                 <div class="list-single-main-item-title fl-wrap">
-                                                                    <h3>Addtional Notes</h3>
+                                                                    <h3>Observação</h3>
                                                                 </div>
                                                                 <textarea cols="40" rows="3" placeholder="Notes"></textarea>
                                                                 <span class="fw-separator"></span>
-                                                                <a  href="#"  class="previous-form action-button back-form   color2-bg">Back</a>
-                                                                <a  href="#"  class="next-form   action-button color-bg">Payment Step</a>
+                                                                <a  href="#"  class="previous-form action-button back-form   color2-bg">Voltar</a>
+                                                                <a  href="#"  class="next-form   action-button color-bg">Pagamento</a>
                                                             </fieldset>
 </template>
